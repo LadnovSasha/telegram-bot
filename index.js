a5d6f93835be34f796597e519567bcb250997662
Oleksandr Ladnov <ladnovsasha@gmail.com>
2018-07-09 17:40:23 +0300

    Add sceleton

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 require('dotenv').config();
 const Telegram = require('node-telegram-bot-api');
 
-const bot = new Telegram(process.env.TOKEN, {polling: true});
+let bot;
+
+if (process.env.WEBHOOK_URL) {
+  bot = new Telegram(process.env.TOKEN, {
+    webHook: {
+      port: 443,
+      key: process.env.KEY,
+      cert: process.env.CERT,
+    },
+  });
+
+  bot.setWebHook(process.env.WEBHOOK_URL, {
+    certificate: process.env.CERT,
+  });
+} else {
+  bot = new Telegram(process.env.TOKEN, { polling: true });
+}
 
 bot.on('message', function(msg) {
-	bot.sendMessage(msg.chat.id, "I've received your message");
+  console.log(msg);
+  bot.sendMessage(msg.chat.id, "I've received your message");
 });
